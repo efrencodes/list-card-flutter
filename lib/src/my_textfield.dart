f71e947913c3dc02b6a895091882eb4886b33405
efrencodes <efrencodes@gmail.com>
2023-02-01 22:51:25 -0600

    ✨ Add todo in listView

--- lib/src/my_textfield.dart
+++ lib/src/my_textfield.dart
@@ -7,12 +7,12 @@ class MyTextfield extends StatefulWidget {
 
 class _MyTextfieldState extends State<MyTextfield> {
   final TextEditingController _controller = TextEditingController();
-  List<String> collections = [];
+  List<String> collections = ['Mandame dinero'];
 
   void onSubmitted (String value) {
     setState(() {
       collections.add(value);
-      _controller.text = '';
+      _controller.clear();
     });
   }
 
@@ -35,7 +35,25 @@ class _MyTextfieldState extends State<MyTextfield> {
           actions: [
             IconButton(
               icon: const Icon(Icons.delete_forever),
-              onPressed: onPressed,
+              onPressed: onPressed
+              // {
+              //   showDialog(
+              //       context: context,
+              //       builder: (BuildContext context) => AlertDialog(
+              //         content: const Text('Delete tasks?'),
+              //         actions: [
+              //           TextButton(
+              //             onPressed: () => Navigator.pop(context, 'Cancel'),
+              //             child: const Text('Cancel'),
+              //           ),
+              //           TextButton(
+              //             onPressed: () => Navigator.pop(context, 'OK'),
+              //             child: const Text('OK'),
+              //           ),
+              //         ],
+              //       )
+              //   );
+              // }
             )
           ],
         ),
@@ -47,7 +65,7 @@ class _MyTextfieldState extends State<MyTextfield> {
                 padding: EdgeInsets.only(top: 25.0, left: 10.0, right: 10.0, bottom: 25.0),
                 child: TextField(
                   controller: _controller,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     label: Text('New tasks'),
                     icon: Icon(Icons.add_circle),
                     iconColor: Colors.red
@@ -55,32 +73,25 @@ class _MyTextfieldState extends State<MyTextfield> {
                   onSubmitted: onSubmitted,
                 )
               ),
-              // ListView(
-              //   children: [
-              //     for(var item in collections)
-              //       ListTile(
-              //         title: Text(
-              //           item,
-              //           style: const TextStyle(
-              //           fontSize: 17.0,
-              //           fontWeight: FontWeight.w400,
-              //           color: Colors.black,
-              //         ),
-              //       ),
-              //     )
-              //   ],
-              // )
-              for(var item in collections)
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-                  child: Text(item,
-                    style: const TextStyle(
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,
-                    ),
-                  ),
-                )
+              Expanded(
+                child: ListView(
+                  children: [
+                    for(var item in collections)
+                      ListTile(
+                        dense: true,
+                        leading: const Icon(Icons.favorite, color: Colors.red),
+                        title: Text(
+                          item,
+                          style: const TextStyle(
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          )
+                      )
+                    )
+                  ]
+                ),
+              ),
             ],
           ),
         ),
